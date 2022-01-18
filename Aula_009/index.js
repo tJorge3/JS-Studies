a3c61494c6876ba01b3a93e00d949410c951045e
Thiago Jorge <79709000+tJorge3@users.noreply.github.com>
2022-01-18 14:34:26 -0300

    Add files via upload

---
+++ Aula_009/index.js
@@ -0,0 +1,15 @@
+// ECMAScript 2015(ES6)
+
+var nome = "Luiz"
+var nome = "Thiago"
+
+// Utilizando o VAR é possível redeclarar uma variável
+
+console.log(nome)
+
+let sobrenome = "Lucas"
+sobrenome = "Jorge"
+
+// Utilizando o let não é possível, mas é possível alterar o seu valor, já que ainda sim é uma variável não constante. Portanto, utilize LET e não VAR
+
+console.log(sobrenome)
